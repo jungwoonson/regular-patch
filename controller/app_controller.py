25e6dc74802ed17ac7f436ffface72249ab6b176
손정운 <juson@e-hcg.com>
2025-03-25 13:15:27 +0900

    refactor: controller 메소드 정리

--- controller/app_controller.py
+++ controller/app_controller.py
@@ -2,32 +2,37 @@ from service.config_reader import ConfigReader
 from service.patch_manager import PatchManager
 from ui.view import PatchGUI
 
-CONFIG_PATH = "./config.json"
-
 class AppController:
-    def __init__(self):
-        self.config_reader = ConfigReader(CONFIG_PATH)
+    def __init__(self, config_path="./config.json"):
+        self.config_reader = ConfigReader(config_path)
         self.patch_manager = PatchManager()
         self.gui = None
 
     def run(self):
-        config_list = self.config_reader.read_config()
-        self.gui = PatchGUI(set_source_callback=self.patch_manager.set_source_dir)
-
-        self.gui.create_config_buttons(config_list)
-        self.gui.init_action_buttons(config_list, self._get_commands())
+        config_list = self._load_config()
+        self.gui = self._create_gui(config_list)
         self.gui.run()
 
+    def _load_config(self):
+        return self.config_reader.read_config()
+
+    def _create_gui(self, config_list):
+        gui = PatchGUI(set_source_callback=self.patch_manager.set_source_dir)
+        gui.create_config_buttons(config_list)
+        gui.init_action_buttons(config_list, self._get_commands())
+        return gui
+
     def _get_commands(self):
+        pm = self.patch_manager
         return {
-            "send_list_import": self.patch_manager.send_patch_list_import,
-            "start_db_patch": self.patch_manager.start_db_patch,
-            "check_server_process": self.patch_manager.check_server_process,
-            "start_server": self.patch_manager.start_server,
-            "stop_server": self.patch_manager.stop_server,
-            "start_server_log": self.patch_manager.start_server_log,
-            "stop_server_log": self.patch_manager.stop_server_log,
-            "deploy_properties": self.patch_manager.deploy_properties,
-            "send_webroot": self.patch_manager.send_webroot,
-            "send_classes": self.patch_manager.send_classes,
+            "send_list_import": pm.send_patch_list_import,
+            "start_db_patch": pm.start_db_patch,
+            "check_server_process": pm.check_server_process,
+            "start_server": pm.start_server,
+            "stop_server": pm.stop_server,
+            "start_server_log": pm.start_server_log,
+            "stop_server_log": pm.stop_server_log,
+            "deploy_properties": pm.deploy_properties,
+            "send_webroot": pm.send_webroot,
+            "send_classes": pm.send_classes,
         }
